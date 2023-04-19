3d49c6a2db7b287add797f71cca47f0fda9f9ae1
Nawed Alam <nawedalam890@gmail.com>
2023-04-19 23:22:45 +0530

    http_request

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -1,5 +1,7 @@
 import 'package:anag_shop/provider/product.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -61,16 +63,30 @@ class Products with ChangeNotifier {
 //   _showFavoritesOnly= false;
 //   notifyListeners();
 // }
-  void addProduct(Product product) {
-    final newProduct = Product(
+  Future<void> addProduct(Product product) {
+     final url =  Uri.https('anag-shop-default-rtdb.firebaseio.com','/products.json');
+      return http.post(url,body: json.encode({
+      'title' : product.title,
+      'description' : product.description,
+      'imageUrl' : product.imageUrl,
+      'price' : product.price,
+      'isFavorite' : product.isFavorite,
+     })).then((response) {
+      //print(json.decode(response.body));
+      final newProduct = Product(
         id: DateTime.now().toString(),
         title: product.title,
         description: product.description,
         price: product.price,
         imageUrl: product.imageUrl);
-_items.add(newProduct);
+    _items.add(newProduct);
     // _items.add(value);
     notifyListeners();
+     }).catchError((error){
+      print(error);
+      throw error;
+     });
+   
   }
   void updateProduct(String id, Product newProduct) {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
